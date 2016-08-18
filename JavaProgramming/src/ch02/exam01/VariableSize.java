93dbc2cea3bc510f2ffc3805596b262055497817
okhyun <joh2787@naver.com>
2016-08-18 14:46:49 +0900

    VariableSize.java 수정함

--- JavaProgramming/src/ch02/exam01/VariableSize.java
+++ JavaProgramming/src/ch02/exam01/VariableSize.java
@@ -13,7 +13,7 @@ public class VariableSize {
 		int var5 = 200000;
 		char var6 = (char) var5;
 		System.out.println(var6);
-		//(캐스팅) 소괄호 안에 들어가있는 타입의 크기만큼 지정한 변수에 타입크기가 적용된다. 
+		//(캐스팅) 소괄호 안에 들어가있는 타입의 크기만큼 지정한 변수에 타입크기가 적용된다.  
 
 	}
 
