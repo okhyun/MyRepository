3f132d6854152a141fa327ba56309c040157c28b
joh2787 <joh2787@naver.com>
2016-08-24 16:41:37 +0900

    과제 추가

---
+++ JavaProgramming/src/ch06/exam11/Student2.java
@@ -0,0 +1,19 @@
+package ch06.exam11;
+
+public class Student2 {
+	//Field
+	int no;
+	String name;
+	int score;
+	
+	//Constructor
+	Student2(){}
+	Student2(int no, String name, int score){
+		this.no = no;
+		this.name = name;
+		this.score = score;
+	}
+	
+	//Method
+
+}
