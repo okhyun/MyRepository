34cb27006803bbf38884b3092b37b785d47f1614
joh2787 <joh2787@naver.com>
2016-10-17 16:36:57 +0900

    10/17일 DB(SQL) 소스파일 추가

---
+++ SqlProgramming/src/ch02/Exam03.java
@@ -0,0 +1,40 @@
+package ch02;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Exam03 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//어떤 JDBC Driver를 사용할 것인가(JDBC Driver 로딩)
+		Class.forName("oracle.jdbc.OracleDriver");
+		//연결 요청을 해서 연결 객체를 얻기
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "tester1", "kosa12345");
+		
+		//매개변수화된 SQL문
+		String sql = "";
+		sql += "select empno, ename, (sal*12+nvl(comm,0)) as yearsal from emp "; 
+		sql += "where sal>? and deptno=? "; 
+		sql += "order by yearsal desc"; 
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, 400);
+		pstmt.setInt(2, 30);
+		ResultSet rs = pstmt.executeQuery(); 
+		
+		while(rs.next()) {
+			int empno = rs.getInt("empno");
+			String ename = rs.getString("ename");
+			int sal = rs.getInt("yearsal");
+			System.out.println(empno + ":" + ename + ":" + sal);
+		}
+		
+		rs.close();
+		pstmt.close();
+		conn.close();
+
+	}
+
+}
---
+++ SqlProgramming/src/ch02/Exam05.java
@@ -0,0 +1,42 @@
+package ch02;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Exam05 {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+			try {
+				Class.forName("oracle.jdbc.OracleDriver");
+				Scanner scanner = new Scanner(System.in);
+				while(true) {
+					System.out.println("검색할 이름:");
+					String name = scanner.nextLine();
+					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "hr", "kosa12345");
+					String sql = "select employee_id, first_name from employees where lower(first_name) like ?";
+					PreparedStatement pstmt = conn.prepareStatement(sql);
+					pstmt.setString(1, "%" + name + "%");
+					ResultSet rs = pstmt.executeQuery(); 
+					while(rs.next()) {
+						String employee_id = rs.getString(1);
+						String first_name = rs.getString(2);
+						System.out.println(employee_id + ":" + first_name);
+					}
+					rs.close();
+					pstmt.close();
+					conn.close();														
+				}
+			} catch (Exception e) {
+				
+				try {conn.close();} catch (SQLException e2) {}
+				e.printStackTrace();
+			}
+
+	}
+
+}
